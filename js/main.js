d6ab1ca470329df5d030cf324c437586a7a4feeb
wei <weiwei198328@gmail.com>
2019-10-07 21:33:45 +0800

    pc页面完成

--- js/main.js
+++ js/main.js
@@ -1,3 +1,35 @@
 $(function() {
-  
+  var indexSwiper = new Swiper(".indexSwiper", {
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    navigation: {
+      nextEl: ".swiper-button-next-1",
+      prevEl: ".swiper-button-prev-1"
+    },
+    pagination: {
+      el: ".swiper-pagination"
+    }
+  });
+
+  var indexSwcol1Swiperiper = new Swiper(".col1Swiper", {
+    loop: true,
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 2,
+    coverflowEffect: {
+      rotate: 0,
+      stretch: 0,
+      depth: 85,
+      modifier: 5,
+      slideShadows: false
+    },
+    navigation: {
+      nextEl: ".swiper-button-next-2",
+      prevEl: ".swiper-button-prev-2"
+    }
+  });
 });
